33bd30a8eece9b1d12a80a2c69ee6edcc94c3049
amanda vikdal <vikdal.amanda@gmail.com>
2022-11-02 10:23:48 -0600

    commit tree c1ac69a8ddb1fae7d7b347b3b72fbec9e18bde74

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
-// Write your code here
+let multiply = 0;
+let num1 = 31;
+let num2 = 2;
+
+multiply = (num1)*(num2);
+
+let random = 0;
+
+random = Math.ceil(Math.random());
+
+let mod = 0;
+let num3 = 24;
+let num4 = 5;
+
+mod = (num3)%(num4);
+
+let max = 0;
+
+max = Math.max(5,10,15,20);
+
+
+
+
